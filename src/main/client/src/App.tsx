3a39b5ee3f6cf2b6d39a9fa5ad5a1c8df7046d5a
omjadas <omja.das@gmail.com>
2020-09-05 14:07:54 +1000

    feat: add header and sign in modal

--- src/main/client/src/App.tsx
+++ src/main/client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Exam } from "./components/exam/exam";
+import { Header } from "./components/header/header";
 import { Subject } from "./components/subject/subject";
 
 export const App = (): JSX.Element => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/subjects/:slug">
             <Subject />
