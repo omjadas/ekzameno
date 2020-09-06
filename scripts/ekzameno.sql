4e8136758cef789412cc0eb9f652161b130f8e69
omjadas <omja.das@gmail.com>
2020-09-06 17:01:59 +1000

    fix: ddl script and jdbc uri

--- scripts/ekzameno.sql
+++ scripts/ekzameno.sql
@@ -3,13 +3,13 @@ CREATE TABLE users (
     email text UNIQUE NOT NULL,
     name text NOT NULL,
     password_hash text NOT NULL,
-    type text NOT NULL,
+    type text NOT NULL
 );
 
 CREATE TABLE subjects (
     id uuid PRIMARY KEY,
     name text NOT NULL,
-    slug text UNIQUE NOT NULL,
+    slug text UNIQUE NOT NULL
 );
 
 CREATE TABLE exams (
@@ -18,7 +18,7 @@ CREATE TABLE exams (
     slug text UNIQUE NOT NULL,
     subject_id uuid NOT NULL REFERENCES subjects ON DELETE CASCADE,
     publish_date timestamp,
-    close_date timestamp,
+    close_date timestamp
 );
 
 CREATE TABLE questions (
@@ -26,38 +26,38 @@ CREATE TABLE questions (
     question text NOT NULL,
     type text NOT NULL,
     marks integer NOT NULL,
-    exam_id uuid NOT NULL REFERENCES exams ON DELETE CASCADE,
+    exam_id uuid NOT NULL REFERENCES exams ON DELETE CASCADE
 );
 
 CREATE TABLE answers (
     id uuid PRIMARY KEY,
     answer text NOT NULL,
     correct boolean NOT NULL,
-    question_id uuid NOT NULL REFERENCES questions ON DELETE CASCADE,
+    question_id uuid NOT NULL REFERENCES questions ON DELETE CASCADE
 );
 
 CREATE TABLE exam_submissions (
     id uuid PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES users ON DELETE CASCADE,
     exam_id uuid NOT NULL REFERENCES exams ON DELETE CASCADE,
-    marks integer,
+    marks integer
 );
 
 CREATE TABLE question_submissions (
     id uuid PRIMARY KEY,
     answer text,
     exam_submission_id uuid NOT NULL REFERENCES exam_submissions ON DELETE CASCADE,
-    question_id uuid NOT NULL REFERENCES questions ON DELETE CASCADE,
+    question_id uuid NOT NULL REFERENCES questions ON DELETE CASCADE
 );
 
 CREATE TABLE instructor_subjects (
     id uuid PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES users ON DELETE CASCADE,
-    subject_id uuid NOT NULL REFERENCES subjects ON DELETE CASCADE,
+    subject_id uuid NOT NULL REFERENCES subjects ON DELETE CASCADE
 );
 
 CREATE TABLE enrolments (
     id uuid PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES users ON DELETE CASCADE,
-    subject_id uuid NOT NULL REFERENCES subjects ON DELETE CASCADE,
+    subject_id uuid NOT NULL REFERENCES subjects ON DELETE CASCADE
 );
