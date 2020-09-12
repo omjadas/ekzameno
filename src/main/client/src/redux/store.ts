0de7fef69a5507c8cb708132f22ee6b51f02329c
omjadas <omja.das@gmail.com>
2020-09-12 14:46:33 +1000

    feat: persist redux store

--- src/main/client/src/redux/store.ts
+++ src/main/client/src/redux/store.ts
@@ -1,15 +1,28 @@
-import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import examsReducer from "./slices/examsSlice";
 import subjectsReducer from "./slices/subjectsSlice";
 import usersReducer from "./slices/usersSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({
+  users: usersReducer,
+  exams: examsReducer,
+  subjects: subjectsReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    exams: examsReducer,
-    subjects: subjectsReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
