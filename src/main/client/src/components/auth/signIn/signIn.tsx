adca302b3bc2b7ec4f16593b8c49a041bd20492d
omjadas <omja.das@datatrue.com>
2020-09-14 13:02:07 +1000

    refactor: use dispatch return to close sign in modal

--- src/main/client/src/components/auth/signIn/signIn.tsx
+++ src/main/client/src/components/auth/signIn/signIn.tsx
@@ -1,3 +1,4 @@
+import { unwrapResult } from "@reduxjs/toolkit";
 import { Formik } from "formik";
 import { FormikControl } from "formik-react-bootstrap";
 import React from "react";
@@ -23,16 +24,17 @@ const FormSchema = yup.object().shape({
 
 export const SignIn = (props: Props): JSX.Element => {
   const dispatch = useAppDispatch();
-  const me = useSelector(selectMe);
 
   const onSubmit = (values: FormValues): void => {
-    dispatch(signIn(values));
+    dispatch(signIn(values))
+      .then(unwrapResult)
+      .then(() => {
+        props.onHide();
+      })
+      .catch(e => {
+        console.error(e);
+      });
   };
-
-  if (me !== undefined) {
-    props.onHide();
-    return <></>;
-  }
 
   return (
     <Tab.Pane eventKey="signIn">
