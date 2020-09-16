4bca55d585e239e4042f058c4e9f4a5ccef6603d
omjadas <omja.das@gmail.com>
2020-09-16 20:14:48 +1000

    chore: create additional users in seeds

--- src/main/java/com/ekzameno/ekzameno/shared/Seeds.java
+++ src/main/java/com/ekzameno/ekzameno/shared/Seeds.java
@@ -11,12 +11,17 @@ import java.util.stream.Collectors;
 
 import com.ekzameno.ekzameno.exceptions.UnknownUserTypeException;
 import com.ekzameno.ekzameno.exceptions.UserAlreadyExistsException;
+import com.ekzameno.ekzameno.models.Subject;
+import com.ekzameno.ekzameno.models.User;
+import com.ekzameno.ekzameno.services.SubjectService;
 import com.ekzameno.ekzameno.services.UserService;
 
 /**
  * Seed the database.
  */
 public class Seeds {
+    private static final UserService userService = new UserService();
+    private static final SubjectService subjectService = new SubjectService();
     private static final String connectionUrl = System.getenv(
         "JDBC_DATABASE_URL"
     );
@@ -39,22 +44,67 @@ public class Seeds {
             e.printStackTrace();
         }
 
-        try (DBConnection connection = DBConnection.getInstance()) {
-            try {
-                new UserService().registerUser(
-                    "Admin",
-                    "admin@ekzame.no",
-                    "password",
-                    "administrator"
-                );
-            } catch (UnknownUserTypeException e) {
-                // this should only occur if the above user type is modified
-                e.printStackTrace();
-            } catch (UserAlreadyExistsException e) {
-                System.out.println("Admin already exists");
-            }
-        } catch (SQLException e) {
+        try {
+            userService.registerUser(
+                "Admin",
+                "admin@ekzame.no",
+                "password",
+                "administrator"
+            );
+
+            User eduardo = userService.registerUser(
+                "Eduardo",
+                "eduardo@ekzame.no",
+                "password",
+                "instructor"
+            );
+
+            User maria = userService.registerUser(
+                "Maria",
+                "mari@ekzame.no",
+                "password",
+                "instructor"
+            );
+
+            User omja = userService.registerUser(
+                "Omja",
+                "odas@ekzame.no",
+                "password",
+                "student"
+            );
+
+            User joao = userService.registerUser(
+                "Joao",
+                "muzamil@ekzame.no",
+                "password",
+                "student"
+            );
+
+            User muzamil = userService.registerUser(
+                "Muzamil",
+                "muzamil@ekzame.no",
+                "password",
+                "student"
+            );
+
+            Subject sda = subjectService.createSubject(
+                "SDA",
+                "Software Design and Architecture",
+                new String[] {
+                    eduardo.getId().toString(),
+                    maria.getId().toString()
+                },
+                new String[] {
+                    omja.getId().toString(),
+                    joao.getId().toString(),
+                    muzamil.getId().toString()
+                }
+            );
+        } catch (UnknownUserTypeException e) {
+            // this should only occur if the above user types are modified
             e.printStackTrace();
+        } catch (UserAlreadyExistsException e) {
+            System.out.println("Users already exist");
         }
     }
 }
