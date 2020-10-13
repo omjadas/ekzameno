fe4263e79819866d842fb70736fe71428a173a4d
João Pereira <joaopereira>
2020-10-13 14:06:49 +0300

    fix: linting error

--- src/main/client/src/components/submission/submissionModal.tsx
+++ src/main/client/src/components/submission/submissionModal.tsx
@@ -87,8 +87,10 @@ export const SubmissionModal = (props: SubmissionModalProps): JSX.Element => {
     <Modal show={props.show} onHide={props.onHide} centered>
       <Modal.Header closeButton>
         <Modal.Title>
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          {student!.name}&apos;s Exam
+          {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            student!.name
+          }&apos;s Exam
         </Modal.Title>
       </Modal.Header>
       <Formik
